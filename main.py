a616d81c9177ea63ae42f568dd9059e30bfa3e69
W. Guilherme <ravok123@gmail.com>
2020-03-06 16:59:02 -0300

    [Udapte] Simplify and Added config variable for custom option

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import os
 import fnmatch
 import sys
 
-comando_ffmpeg = 'ffmpeg'
-codecVideo = '-c:v libx264'
-crf = '-crf 23'
-preset = '-preset fast'
-codecAudio = '-c:a aac'
-bitrateAudio = '-b:a 320k'
+codecVideo = '-c:v libx264 '
+crf = '-crf 23 '
+preset = '-preset fast '
+codecAudio = '-c:a aac '
+bitrateAudio = '-b:a 320k '
 newExtension = '.mp4'
+
+config = codecVideo + crf + preset + codecAudio + bitrateAudio
 
 fromPath = '/Users/wguilherme/Downloads/records'
 
 for root, folders, files in os.walk(fromPath):
     for file in files:
-        if file.endswith(('.mov', '.mp4')):
+        if file.endswith(('.mov')):
             fullPath = os.path.join(root, file)
             fileName, fileExtension = os.path.splitext(file)
             newFileName = fileName + '_converted' + newExtension
             exportFile = os.path.join(root, newFileName)
-            command = f'{comando_ffmpeg} -i "{fullPath}" ' \
-                f'{codecVideo} {crf} {preset} {codecAudio} {bitrateAudio} "{exportFile}"'
+            command = f'ffmpeg -i "{fullPath}" {config} "{exportFile}"'
+            print(command)
             os.system(command)
